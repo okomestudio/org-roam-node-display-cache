bb476379a86c8d5c9389a9263442ce7085b2d774
Taro Sato <okomestudio@gmail.com>
2025-06-16 22:54:04 -0700

    Use file as cache key

--- org-roam-node-display-cache.el
+++ org-roam-node-display-cache.el
@@ -4,9 +4,9 @@
 ;;
 ;; Author: Taro Sato <okomestudio@gmail.com>
 ;; URL: https://github.com/okomestudio/org-roam-node-display-cache/
-;; Version: 0.1.2
+;; Version: 0.2.1
 ;; Keywords:
-;; Package-Requires: ((emacs \"26.1\") (org-roam "20250527.1558") (ok "0.2.3"))
+;; Package-Requires: ((emacs \"26.1\") (org-roam "20250527.1558") (ok "0.5.2"))
 ;;
 ;;; License:
 ;;
@@ -37,23 +37,25 @@
 
 (defun org-roam-node-display-cache--get (node total-width)
   "Get the cached item for NODE and TOTAL-WIDTH from in-memory cache."
-  (if-let* ((id (org-roam-node-id node))
+  (if-let* ((file (org-roam-node-file node))
+            (id (org-roam-node-id node))
             (title (org-roam-node-title node))
-            (entries (gethash id org-roam-node-display-cache--cache))
-            (value (cdr (assoc `(,title ,total-width) entries))))
+            (entries (gethash file org-roam-node-display-cache--cache))
+            (value (cdr (assoc `(,id ,title ,total-width) entries))))
       value))
 
 (defun org-roam-node-display-cache--save (value node total-width)
   "Save VALUE to in-memory cache for NODE and TOTAL-WIDTH."
-  (let* ((id (org-roam-node-id node))
+  (let* ((file (org-roam-node-file node))
+         (id (org-roam-node-id node))
          (title (org-roam-node-title node))
-         (entries (gethash id org-roam-node-display-cache--cache)))
-    (push (cons `(,title ,total-width) value) entries)
-    (puthash id entries org-roam-node-display-cache--cache)))
+         (entries (gethash file org-roam-node-display-cache--cache)))
+    (push (cons `(,id ,title ,total-width) value) entries)
+    (puthash file entries org-roam-node-display-cache--cache)))
 
 (defun org-roam-node-display-cache--remove (node)
   "Remove NODE item from the in-memory cache."
-  (remhash (org-roam-node-id node) org-roam-node-display-cache--cache))
+  (remhash (org-roam-node-file node) org-roam-node-display-cache--cache))
 
 (defun org-roam-node-display-cache--clear ()
   "Clear the in-memory cache."
